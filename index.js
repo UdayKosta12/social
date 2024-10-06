999fe03ebc6c1c89560739009d50a04b5bd76513
UdayKosta12 <udaykushvanshi12@gmail.com>
2024-10-06 16:05:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function testResults(form){
+  var a = form.username.value;
+  var b = form.password.value;
+//   alert("username :" +a)
+console.log("username:"+ a);
+console.log("password:"+b);
+}
